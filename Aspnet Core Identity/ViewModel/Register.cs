29ac17dcf24a3e5dfc26c04d507c36abdc7804a3
Godsent Ukomadu <Godsentukomadu@outlook.com>
2023-06-01 10:32:51 +0100

    Add project files.

---
+++ Aspnet Core Identity/ViewModel/Register.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aspnet_Core_Identity.ViewModel
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="Password Does not Match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
